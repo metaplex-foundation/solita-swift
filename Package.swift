361a6be186437f7014075135f9be8d156d662bff
Arturo Jamaica <me@arturojamaica.com>
2022-08-12 11:40:10 -0500

    Metaplex foundation

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "Solana", url: "git@github.com:ajamaica/Solana.Swift.git", from: "1.2.0"),
+        .package(name: "Solana", url: "git@github.com:metaplex-foundation/Solana.Swift.git", from: "1.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
